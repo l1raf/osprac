ea6cb37fa17c09db0126995097701cdd6b973553
Sabiyat <kssabiyat@gmail.com>
2021-02-22 16:09:50 +0300

    Add lab6-1

---
+++ lab6-1.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int fd[2], size, bSize;
+
+  if (pipe2(fd, O_NONBLOCK) < 0) 
+  {
+    printf("Can\'t open pipe\n");
+    exit(-1);
+  }
+
+  do
+  {
+    size = write(fd[1], "a", 2);
+    bSize += 2;
+  } 
+  while (size > 0);
+
+  printf("Buffer size: %d\n", bSize);
+}
